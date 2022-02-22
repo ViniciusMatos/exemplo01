675a444b3cd9b9289d2957bb6e24750f4e97c7c7
ViniciusMatos <vmatos12@gmail.com>
2022-02-22 17:40:56 -0300

    Adicionando nova linha no codigo

--- src/exemplo01/Exemplo.java
+++ src/exemplo01/Exemplo.java
@@ -3,6 +3,8 @@ package exemplo01;
 public class Exemplo {
 	public static void main(String args[]) {
 		System.out.println("Hello World");
+		
+		System.out.println("Programa funcionando!");
 	}
 
 }
